5e41b8813dd779e260de470fc7878f33a261eaa5
gabrielamonaro <gabrielamonaro@outlook.com>
2026-02-13 12:19:27 -0300

    feat: static form mechanism

--- src/app/components/product-form/product-form.ts
+++ src/app/components/product-form/product-form.ts
@@ -2,16 +2,23 @@ import { Component, Input } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
-  imports: [InputTextModule, ButtonModule, RatingModule],
+  imports: [InputTextModule, ButtonModule, RatingModule, FormsModule],
   templateUrl: './product-form.html',
   styleUrl: './product-form.scss',
   standalone: true,
 })
 export class ProductForm {
+  product = {
+    name: '',
+    price: 0,
+    image: '',
+    rating: 0,
+  };
   submitForm() {
-    console.log('Form submit chamado!');
+    console.log('Dados do formulário: ', this.product);
   }
 }
